2537d270e46e518a4d2bb310dedae64d03e83789
Sam Cox <sam.cox@nottingham.ac.uk>
2019-09-24 11:03:34 +0100

    Create separate graphs for each of the base conditions.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sys
 import os
 import itertools
+import visualisedictionary as vd
 
 ifdefs = []
 ifndefs = []
 endifs = []
 elifs = []
 elseifs = []
-ifdefineds = []
+ifs = []
 
 
 def combine(args):
@@ -54,9 +55,9 @@ def count_and_capture_elseifs(line, file, line_number):
         elseifs.append((line, file, line_number))
 
 
-def count_and_capture_ifdefineds(line, file, line_number):
-    if line.strip().startswith('#if defined'):
-        ifdefineds.append((line, file, line_number))
+def count_and_capture_ifs(line, file, line_number):
+    if line.strip().startswith('#if '):
+        ifs.append((line, file, line_number))
 
 
 def get_all_cpp_beneath(root):
@@ -73,11 +74,11 @@ def get_all_directives_in_file(file):
         line = fh.readline()
         line_number = 0
         while line:
-            line = line.strip('\n')
+            line = line.split(sep='//')[0].strip()
             count_and_capture_ifdefs(line, file, line_number)
             count_and_capture_ifndefs(line, file, line_number)
             count_and_capture_endifs(line, file, line_number)
-            count_and_capture_ifdefineds(line, file, line_number)
+            count_and_capture_ifs(line, file, line_number)
             count_and_capture_elifs(line, file, line_number)
             count_and_capture_elseifs(line, file, line_number)
             line = fh.readline()
@@ -85,11 +86,26 @@ def get_all_directives_in_file(file):
     return
 
 
+def add_to_dict(full_list,current_dict):
+    # print('full list', full_list)
+    # print('current_dict', current_dict)
+    if len(full_list) == 1:
+        if full_list[0] not in current_dict:
+            current_dict[full_list[0]] = dict()
+    else:
+        current_dict[full_list[0]] = add_to_dict(full_list[1:],current_dict[full_list[0]])
+    return current_dict
+
+
 if __name__ == "__main__":
     root = sys.argv[1]
+    full_dict = dict()
 
     for file in get_all_cpp_beneath(root):
-        print(file)
+        current_list = []
+        test_dict = dict()
+
+        print('\n'+file)
         get_all_directives_in_file(file)
 
         filtered_ifdefs = get_x_from_file_y(ifdefs, file)
@@ -97,18 +113,68 @@ if __name__ == "__main__":
         filtered_endifs = get_x_from_file_y(endifs, file)
         filtered_elifs = get_x_from_file_y(elifs, file)
         filtered_elseifs = get_x_from_file_y(elseifs, file)
-        filtered_ifdefineds = get_x_from_file_y(ifdefineds, file)
+        filtered_ifs = get_x_from_file_y(ifs, file)
 
+        filtered_ifdefs = [x for x in filtered_ifdefs if not x[0] == '#ifdef OLD_DYNAMICS']
+        filtered_ifndefs = [x for x in filtered_ifndefs if not x[0] == '#ifndef OLD_DYNAMICS']
         indent = 0
         for tup in combine((filtered_ifdefs,
                             filtered_ifndefs,
                             filtered_endifs,
                             filtered_elifs,
                             filtered_elseifs,
-                            filtered_ifdefineds)):
+                            filtered_ifs)):
 
             if tup[0].startswith('#endif'):
                 indent -= 1
-            print(str(indent) + indent*4*' ' + tup[0])
-            if tup[0].startswith(('#ifdef', '#ifndef', '#if defined')):
+                try:
+                    current_list.pop()
+                except:
+                    print('error in ', tup[0], tup[1], tup[2])
+            # print(str(indent) + indent*4*' ' + tup[0])
+            if tup[0].startswith(('#ifdef', '#if ', '#ifndef')):
                 indent += 1
+                current_list.append(tup[0])
+                add_to_dict(current_list,test_dict)
+                add_to_dict(current_list,full_dict)
+            if tup[0].startswith(('#elif', '#elseif')):
+                current_list.pop()
+                current_list.append(tup[0])
+                add_to_dict(current_list, test_dict)
+                add_to_dict(current_list, full_dict)
+        print(current_list)
+        print(test_dict)
+        vd.pprint(test_dict)
+        G = vd.KeysGraph(test_dict)
+        G.draw(file + 'test.png')
+        for tup in combine((filtered_ifdefs,
+                            filtered_ifndefs,
+                            filtered_endifs,
+                            filtered_elifs,
+                            filtered_elseifs,
+                            filtered_ifs)):
+
+            if tup[0].startswith('#endif'):
+                try:
+                    current_list.pop()
+                except:
+                    print('error in ', tup[0], tup[1], tup[2])
+            # print(str(indent) + indent*4*' ' + tup[0])
+            if tup[0].startswith(('#ifdef', '#if ', '#ifndef')):
+                current_list.append(tup[0])
+                add_to_dict(current_list,test_dict)
+                add_to_dict(current_list,full_dict)
+            if tup[0].startswith(('#elif', '#elseif')):
+                current_list.pop()
+                current_list.append(tup[0])
+                add_to_dict(current_list, test_dict)
+                add_to_dict(current_list, full_dict)
+
+        assert len(current_list) == 0, AssertionError('current_list is not empty at end of file processing')
+    vd.pprint(full_dict)
+    G = vd.KeysGraph(full_dict)
+    G.graph_attr.update(size="100,100")
+    # G.graph_attr.update(ratio="fill")
+    G.draw('./fulltest.png')
+
+    # TODO: handle else clauses
